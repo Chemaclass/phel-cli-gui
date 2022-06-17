4d0df7e5f003e3644be66a24583f6c2e8184a924
Chemaclass <chemaclass@outlook.es>
2022-06-17 23:34:53 +0200

    Create a WIP idea for a snake game in phel

--- src/php/TerminalGui.php
+++ src/php/TerminalGui.php
@@ -5,7 +5,10 @@ declare(strict_types=1);
 namespace PhelCliGui;
 
 use Symfony\Component\Console\Cursor;
+use Symfony\Component\Console\Formatter\OutputFormatter;
+use Symfony\Component\Console\Input\StreamableInputInterface;
 use Symfony\Component\Console\Output\BufferedOutput;
+use Symfony\Component\Console\Output\OutputInterface;
 
 final class TerminalGui
 {
@@ -15,10 +18,41 @@ final class TerminalGui
     private int $width = 0;
     private int $height = 0;
 
-    public function __construct()
+    public function __construct(?OutputFormatter $formatter = null)
     {
         $this->output = new BufferedOutput();
+        $this->output->setFormatter($formatter ?? new OutputFormatter());
+
         $this->cursor = new Cursor($this->output);
+        $this->cursor->hide();
+    }
+
+    public function __destruct()
+    {
+        $this->cursor->show();
+    }
+
+    public function board(int $width, int $height, $inputStream = STDIN): void
+    {
+        $this->width = $width;
+        $this->height = $height;
+
+        stream_set_blocking($inputStream, false);
+        $sttyMode = shell_exec('stty -g');
+        shell_exec('stty -icanon -echo');
+
+        $borderLine = implode('', array_fill(0, $width - 2, '─'));
+        $emptyLine = implode('', array_fill(0, $width - 2, ' '));
+
+        $out = "┌{$borderLine}┐" . PHP_EOL;
+        $out .= str_repeat("│{$emptyLine}│" . PHP_EOL, $height - 2);
+        $out .= "└{$borderLine}┘" . PHP_EOL;
+
+        $this->cursor->moveToPosition(0, 0);
+        $this->write($out);
+
+        stream_set_blocking($inputStream, true);
+        shell_exec(sprintf('stty %s', $sttyMode));
     }
 
     public function clearScreen(): void
@@ -38,22 +72,6 @@ final class TerminalGui
         $this->write($text);
         $this->cursor->moveToPosition(0, $this->height);
         $this->write(PHP_EOL);
-    }
-
-    public function board(int $width, int $height): void
-    {
-        $this->width = $width;
-        $this->height = $height;
-
-        $borderLine = implode('', array_fill(0, $width - 2, '─'));
-        $emptyLine = implode('', array_fill(0, $width - 2, ' '));
-
-        $out = "┌{$borderLine}┐" . PHP_EOL;
-        $out .= str_repeat("│{$emptyLine}│" . PHP_EOL, $height - 2);
-        $out .= "└{$borderLine}┘" . PHP_EOL;
-
-        $this->cursor->moveToPosition(0, 0);
-        $this->write($out);
     }
 
     private function write(string $text): void
