27fa2343346b5a5f7c20357cbc5ea46dc3b29d95
Chemaclass <chemaclass@outlook.es>
2023-05-23 21:37:53 +0200

    Upgrade phel-config from array to PhelConfig object

--- phel-config.php
+++ phel-config.php
@@ -2,16 +2,20 @@
 
 declare(strict_types=1);
 
-return [
-    'src-dirs' => ['src/phel/'],
-    'test-dirs' => ['tests/phel/'],
-    'vendor-dir' => 'vendor',
-    'export' => [
-        'directories' => ['src/modules'],
-        'namespace-prefix' => 'PhelGenerated',
-        'target-directory' => 'src/PhelGenerated',
-    ],
-    'ignore-when-building' => [
-        'test-keyboard.phel',
-    ],
-];
+use Phel\Config\PhelConfig;
+use Phel\Config\PhelExportConfig;
+
+return (new PhelConfig())
+    ->setSrcDirs(['src/phel/'])
+    ->setTestDirs(['tests/phel/'])
+    ->setVendorDir('vendor')
+    ->setOutDir('out')
+    ->setExport(
+        (new PhelExportConfig())
+            ->setDirectories(['src/modules'])
+            ->setNamespacePrefix('PhelGenerated')
+            ->setTargetDirectory('src/PhelGenerated')
+    )
+    ->setIgnoreWhenBuilding(['local.phel'])
+    ->setKeepGeneratedTempFiles(false)
+    ->setIgnoreWhenBuilding(['test-keyboard.phel']);
