9a9ede8860d1ebbb92de66ef67eabd1bf3fc5735
Chemaclass <chemaclass@outlook.es>
2024-06-22 07:44:55 +0200

    update phel-config.php

--- phel-config.php
+++ phel-config.php
@@ -2,22 +2,17 @@
 
 declare(strict_types=1);
 
+use Phel\Config\PhelBuildConfig;
 use Phel\Config\PhelConfig;
-use Phel\Config\PhelExportConfig;
-use Phel\Config\PhelOutConfig;
 
 return (new PhelConfig())
     ->setSrcDirs(['src/phel/'])
     ->setTestDirs(['tests/phel/'])
     ->setVendorDir('vendor')
-    ->setOut((new PhelOutConfig())
-        ->setDestDir('out'))
-    ->setExport(
-        (new PhelExportConfig())
-            ->setDirectories(['src/modules'])
-            ->setNamespacePrefix('PhelGenerated')
-            ->setTargetDirectory('src/PhelGenerated')
-    )
-    ->setIgnoreWhenBuilding(['local.phel','test-keyboard.phel'])
+    ->setFormatDirs(['src', 'tests'])
+    ->setBuildConfig((new PhelBuildConfig())
+        ->setMainPhpPath('out/main.php')
+        ->setMainPhelNamespace('phel-cli-gui\terminal-gui'))
+    ->setIgnoreWhenBuilding(['local.phel', 'test-keyboard.phel'])
     ->setKeepGeneratedTempFiles(false)
 ;
