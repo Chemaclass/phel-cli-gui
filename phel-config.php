909e8e3970ec05cfb111b52c153a4f945a012296
Chemaclass <chemaclass@outlook.es>
2026-06-14 15:40:14 +0200

    chore: use non-deprecated PhelConfig builder API

--- phel-config.php
+++ phel-config.php
@@ -4,9 +4,10 @@ declare(strict_types=1);
 
 use Phel\Config\PhelBuildConfig;
 use Phel\Config\PhelConfig;
+use Phel\Config\ProjectLayout;
 
 return (new PhelConfig())
-    ->useNestedLayout()
-    ->setBuildConfig((new PhelBuildConfig())
-        ->setMainPhpPath('out/main.php')
-        ->setMainPhelNamespace('phel-cli-gui\terminal-gui'));
+    ->withLayout(ProjectLayout::Nested)
+    ->withBuildConfig((new PhelBuildConfig())
+        ->withMainPhpPath('out/main.php')
+        ->withMainPhelNamespace('phel-cli-gui\terminal-gui'));
